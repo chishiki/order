af9e194cbf2dd88ba3c1507175042f516ff53f0d
Christopher Webb <chishiki@gmail.com>
2021-10-05 23:53:08 +0900

    update order sql

--- data/order-schema.sql
+++ data/order-schema.sql
@@ -1,34 +1,7 @@
 
+DROP TABLE IF EXISTS `order_Order`;
 
-
-DROP TABLE IF EXISTS `order_Customer`;
-
-CREATE TABLE `order_Customer` (
-  `customerID` int(12) NOT NULL AUTO_INCREMENT,
-  `siteID` int(12) NOT NULL,
-  `creator` int(12) NOT NULL,
-  `created` datetime NOT NULL,
-  `updated` datetime NOT NULL,
-  `deleted` int(1) NOT NULL,
-  `customerNameEnglish` varchar(100) NOT NULL,
-  `customerNameJapanese` varchar(100) NOT NULL,
-  `customerPersonInCharge` varchar(100) NOT NULL,
-  `customerRepresentativeDepartment` varchar(100) NOT NULL,
-  `customerRepresentativeTitle` varchar(100) NOT NULL,
-  `customerHonorarySuffix` varchar(10) NOT NULL,
-  `customerNotes` varchar(255) NOT NULL,
-  `customerTelephone` varchar(50) NOT NULL,
-  `customerFax` varchar(50) NOT NULL,
-  `customerEmail` varchar(50) NOT NULL,
-  `priceLevel` int(2) NOT NULL,
-  PRIMARY KEY (`customerID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-
-
-DROP TABLE IF EXISTS `order_SalesOrder`;
-
-CREATE TABLE `order_SalesOrder` (
+CREATE TABLE `order_Order` (
   `salesOrderID` int(12) NOT NULL AUTO_INCREMENT,
   `siteID` int(12) NOT NULL,
   `creator` int(12) NOT NULL,
@@ -54,9 +27,9 @@ CREATE TABLE `order_SalesOrder` (
 
 
 
-DROP TABLE IF EXISTS `order_SalesOrderDetail`;
+DROP TABLE IF EXISTS `order_OrderDetail`;
 
-CREATE TABLE `order_SalesOrderDetail` (
+CREATE TABLE `order_OrderDetail` (
   `salesOrderDetailID` int(12) NOT NULL AUTO_INCREMENT,
   `siteID` int(12) NOT NULL,
   `creator` int(12) NOT NULL,
@@ -74,33 +47,3 @@ CREATE TABLE `order_SalesOrderDetail` (
   `descriptionOfPriceList` varchar(100) NOT NULL,
   PRIMARY KEY (`salesOrderDetailID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-
-
-DROP TABLE IF EXISTS `order_Product`;
-
-CREATE TABLE `order_Product` (
-  `productID` int(12) NOT NULL AUTO_INCREMENT,
-  `siteID` int(12) NOT NULL,
-  `creator` int(12) NOT NULL,
-  `created` datetime NOT NULL,
-  `updated` datetime NOT NULL,
-  `deleted` int(1) NOT NULL,
-  `productName` varchar(100) NOT NULL,
-  `productNumber` int(20) NOT NULL,
-  `productDescriptionEnglish` varchar(255) NOT NULL,
-  `productDescriptionJapanese` varchar(255) NOT NULL,
-  `productUnitPriceYen1` decimal(13,4) NOT NULL,
-  `productUnitPriceYen2` decimal(13,4) NOT NULL,
-  `productUnitPriceYen3` decimal(13,4) NOT NULL,
-  `productUnitPriceYen4` decimal(13,4) NOT NULL,
-  `productUnitPriceDollar` decimal(13,4) NOT NULL,
-  `usCustomerUnitPrice` decimal(13,4) NOT NULL,
-  `descriptionOfPriceList` varchar(100) NOT NULL,
-  `productNotes` text NOT NULL,
-  `productUsesSerialNumber` int(1) NOT NULL,
-  `productType` varchar(10) NOT NULL,
-  PRIMARY KEY (`productID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-
