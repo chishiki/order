779ed889e169c6349212db5aa132df8219faf64d
Christopher Webb <chishiki@gmail.com>
2021-10-11 12:24:55 +0900

    update order and order detail schema

--- data/order-schema.sql
+++ data/order-schema.sql
@@ -2,27 +2,26 @@
 DROP TABLE IF EXISTS `order_Order`;
 
 CREATE TABLE `order_Order` (
-  `salesOrderID` int(12) NOT NULL AUTO_INCREMENT,
-  `siteID` int(12) NOT NULL,
-  `creator` int(12) NOT NULL,
+  `orderID` int NOT NULL AUTO_INCREMENT,
+  `siteID` int NOT NULL,
+  `creator` int NOT NULL,
   `created` datetime NOT NULL,
   `updated` datetime NOT NULL,
-  `deleted` int(1) NOT NULL,
-  `salesOrderDate` date NOT NULL,
+  `deleted` int NOT NULL,
+  `orderDate` date NOT NULL,
   `status` varchar(10) NOT NULL,
-  `estimateID` int(12) NOT NULL,
   `projectName` varchar(100) NOT NULL,
   `customerOrderNo` varchar(30) NOT NULL,
-  `customerID` int(12) NOT NULL,
-  `customerPersonInCharge` varchar(30) NOT NULL,
+  `customerID` int NOT NULL,
+  `customerRepresentative` varchar(30) NOT NULL,
   `customerContact` varchar(50) NOT NULL,
-  `salesOrderNotes` text NOT NULL,
-  `salesOrderFinal` int(1) NOT NULL,
-  `salesOrderDiscount` decimal(13,4) NOT NULL,
-  `salesOrderDiscountYen` decimal(13,4) NOT NULL,
-  `salesOrderSubtotalYen` decimal(13,4) NOT NULL,
-  `salesOrderTotalYen` decimal(13,4) NOT NULL,
-  PRIMARY KEY (`salesOrderID`)
+  `orderNotes` text NOT NULL,
+  `orderFinal` int NOT NULL,
+  `orderDiscountRate` decimal(13,4) NOT NULL,
+  `orderDiscount` decimal(13,4) NOT NULL,
+  `orderSubtotal` decimal(13,4) NOT NULL,
+  `orderTotal` decimal(13,4) NOT NULL,
+  PRIMARY KEY (`orderID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
@@ -30,20 +29,17 @@ CREATE TABLE `order_Order` (
 DROP TABLE IF EXISTS `order_OrderDetail`;
 
 CREATE TABLE `order_OrderDetail` (
-  `salesOrderDetailID` int(12) NOT NULL AUTO_INCREMENT,
-  `siteID` int(12) NOT NULL,
-  `creator` int(12) NOT NULL,
+  `orderDetailID` int NOT NULL AUTO_INCREMENT,
+  `siteID` int NOT NULL,
+  `creator` int NOT NULL,
   `created` datetime NOT NULL,
   `updated` datetime NOT NULL,
-  `deleted` int(1) NOT NULL,
-  `salesOrderID` int(12) NOT NULL,
-  `salesOrderDetailProductID` int(12) NOT NULL,
-  `salesOrderDetailProductDescription` varchar(255) NOT NULL,
-  `salesOrderDetailQuantity` int(12) NOT NULL,
-  `salesOrderDetailProductUnitPriceDollar` decimal(13,4) NOT NULL,
-  `salesOrderDetailProductUnitPriceYen` decimal(13,4) NOT NULL,
-  `salesOrderDetailYen` decimal(13,4) NOT NULL,
-  `salesOrderDetailDollar` decimal(13,4) NOT NULL,
-  `descriptionOfPriceList` varchar(100) NOT NULL,
-  PRIMARY KEY (`salesOrderDetailID`)
+  `deleted` int NOT NULL,
+  `orderID` int NOT NULL,
+  `orderDetailProductID` int NOT NULL,
+  `orderDetailProductDescription` varchar(255) NOT NULL,
+  `orderDetailQuantity` int NOT NULL,
+  `orderDetailProductUnitPrice` decimal(13,4) NOT NULL,
+  `orderDetailPrice` decimal(13,4) NOT NULL,
+  PRIMARY KEY (`orderDetailID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
